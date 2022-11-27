456acb1c8c1293a7226edfb4db65957aaa99ca0a
Mohammed Ammar <imohammedomz@gmail.com>
2022-11-28 01:37:49 +0300

    Spawning Enemies

---
+++ Die die die/Assets/scripts/enemySpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawn : MonoBehaviour
+{
+    public GameObject[] enemies;
+    public float timer = 0;
+    public float start = 10f;
+    
+    private int random;
+    void Start()
+    {
+        timer = start;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= 1 *Time.deltaTime;
+        if(timer <= 0) {
+            for(int i = 0; i < 3; i++) {
+                random = Random.Range(0,30);
+                Instantiate(enemies[random],new Vector3(Random.Range(-9,10),Random.Range(-5,6),0),Quaternion.identity);
+            }
+            timer = start;
+        }
+    }
+}
--- Die die die/Assets/scripts/movement.cs
+++ Die die die/Assets/scripts/movement.cs
@@ -2,19 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+    public GameObject target;
+    
+
+    
     void Update()
     {
-       if(gameObject.tag == "Player"){
-        Debug.Log("player moving");
-       } 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        if(movement.x>0 || movement.y > 0){
+            animator.SetBool("isMoving", true);
+        }
+        else{
+            animator.SetBool("isMoving", false);
+        }
+
+         
+   }
+
+    void FixedUpdate()
+    {
+        if(gameObject.tag == "Player") {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
+        else{
+            transform.position = Vector2.MoveTowards(transform.position,target.transform.position,3*Time.deltaTime);
+        }
+
     }
+        private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Debug.Log("GameOver");
+            Destroy(gameObject);
+        }
+}
 }
